9d8e3ac7517cb51282174213388d228ee0229d4f
seannse <seannse@gmail.com>
2023-02-06 16:17:25 +0200

    props

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -42,13 +42,15 @@ const Profile = (props) => {
 };
 
 Profile.propTypes = {
-    username: PropTypes.string,
-    tag: PropTypes.string,
-    location: PropTypes.string,
-    avatar: PropTypes.string,
-    followers: PropTypes.number,
-    views: PropTypes.number,
-    likes: PropTypes.number,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
 
 export default Profile;
